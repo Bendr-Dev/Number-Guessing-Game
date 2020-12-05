edccd796ff74d07c2b8eefe0752669a4a23380b3
Brandon Endres <17bendres@gmail.com>
2020-12-04 20:39:49 -0500

    Fixes unexpected results for inputs

--- index.php
+++ index.php
@@ -1,37 +1,39 @@
 <?php
     // Variable declaration
-    $prevEntries;
-    $numEntriesLeft;
-    $secretCode;
-    $game;
-
+    $prevEntries = array();
+    $numEntriesLeft = 10;
+    $secretCode = [5, 6, 8, 1];
+    $game = array("numEntriesLeft"=>$numEntriesLeft, "prevEntries"=>$prevEntries, "currentEntry"=>array());
+    
     /*
-     *  Generates array of 4 random values spanning from 0 through 9
-     */
+    *  Generates array of 4 random values spanning from 0 through 9
+    */
     function generateRandomValues() {
         return array(rand(0,9), rand(0,9), rand(0,9), rand(0,9));
     }
 
     /*
-     *  Initializes / resets game by assigning variables
-     */
-    if (isset($_POST["reset"])) {
+    *  Initializes / resets game by assigning variables
+    */
+    if (isset($_POST["restart"])) {
         $prevEntries = array();
         $numEntriesLeft = 10;
-        $secretCode = generateRandomValues();
+        $secretCode = [5, 6, 8, 0];
         $game = array("numEntriesLeft"=>$numEntriesLeft, "prevEntries"=>$prevEntries, "currentEntry"=>array());
 
         returnJsonResponse( $game ); // return response
     }
 
+
+    // MOVE THIS TO SEPERATE PHP FILE
     /*
-     *  Determine if code sent from user matches randomly generated code
-     */
+    *  Determine if code sent from user matches randomly generated code
+    */
     if (isset($_POST["submit"])) {
         // Get data from client, update game
-        $userCode = $_POST['submit'];
-        $numEntriesLeft -= 1;
-        $prevEntries.array_push($userCode);
+        $userCode = [intval($_POST["value1"]), intval($_POST["value2"]),  intval($_POST["value3"]),  intval($_POST["value4"])];
+        --$game["numEntriesLeft"];
+        array_push($prevEntries, $userCode);
         $codeResult = array("green"=>array(), "yellow"=>array(), "red"=> array());
         
         // Counts for duplicate numbers
@@ -42,14 +44,14 @@
         // Second check - does not match location-wise, but number is found in different location
         // Default - No match found
         for ($x = 0; $x < count($userCode); $x++) {
-            if ($userCode[$x] === $secretCode[$x]) { // Matches
-                $codeResult["green"].array_push($userCode[$x]);
-                $secretCodeCount["${$secretCode[$x]}"] -= 1;
-            } elseif ($userCode[$x] !== $secretCode[$x] && $secretCodeCount["${$secretCode[$x]}"] > 0) {
-                $codeResult["yellow"].array_push($userCode[$x]);
-                $secretCodeCount["${$secretCode[$x]}"] -= 1;
+            if ($userCode[$x] === $secretCode[$x]) { // Matches                
+                array_push($codeResult["green"], $userCode[$x]);
+                --$secretCodeCount[$secretCode[$x]];
+            } elseif ($userCode[$x] !== $secretCode[$x] && array_key_exists(strval($userCode[$x]), $secretCodeCount) && $secretCodeCount[strval($userCode[$x])] > 0) {
+                array_push($codeResult["yellow"], $userCode[$x]);
+                --$secretCodeCount[strval($userCode[$x])];
             } else {
-                $codeResult["red"].array_push($userCode[$x]);
+                array_push($codeResult["red"], $userCode[$x]);
             }
         }
 
@@ -59,8 +61,8 @@
     }
 
     /*
-     *  Returns data encoded in JSON format
-     */
+    *  Returns data encoded in JSON format
+    */
     function returnJsonResponse( $data ) {
         // Clean up header and re-instantiate content-type
         header_remove();
@@ -68,8 +70,5 @@
 
         // return PHP object as JSON
         echo json_encode( $data );
-
-        // Exit script
-        exit();
     }
 ?>
